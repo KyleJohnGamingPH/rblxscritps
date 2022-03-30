9ba28e103f03fd1bb911518882eb9b541759c4f2
KyleJohnGamingPH <102275738+KyleJohnGamingPH@users.noreply.github.com>
2022-03-30 08:37:31 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Te
 
 local PlaceId = game.PlaceId
 
-if PlaceId == 2753915549 then
+PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/joi-droid/MaruHubBF/main/GOHANSSJ3'))()
   
   else
