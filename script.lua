24397d39c73062f0fdc539e5a776eafb5936c24b
KyleJohnGamingPH <102275738+KyleJohnGamingPH@users.noreply.github.com>
2022-03-31 18:39:18 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,6 +7,8 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ProfessionalDoge/Connectors/main/Blok_frut_connector.lua"))()
 elseif PlaceId == 142823291 then
   loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+elseif PlaceId == 2788229376 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua%27))()
 
  else
   
