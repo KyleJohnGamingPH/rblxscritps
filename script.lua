e86781b6f980cfadcb9ac1b61e8638db33051dd1
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-04-04 15:19:18 +0800

    Update script.lua
    New: Added Ro-Ghoul Script
    Fixes: Fixed unexecutable Da-Hood script by replacing ' to " on line 11

--- script.lua
+++ script.lua
@@ -1,17 +1,20 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Compiled", Text = "By Kyle.#3427"})
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Text = "1.0"})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Text = "ProfessionalDoge Patch 1.1"})
 
 local PlaceId = game.PlaceId
 
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SHAREHACK/bloxfruit/main/free"))()
+	@@ -8,10 +8,14 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 elseif PlaceId == 142823291 then
   loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
 elseif PlaceId == 2788229376 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua"))()
 elseif PlaceId == 914010731 then
   loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua")))()
+ elseif PlaceId == 914010731 then
+  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua"))()
 
  else
-  
+
+  game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Sorry", Text = "game currently unsupported"})
+
  end
