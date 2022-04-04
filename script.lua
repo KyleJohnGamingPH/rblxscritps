6d413e1bf72643579febdbb336bb45061fbd3656
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-04-04 15:20:30 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,7 +8,7 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 elseif PlaceId == 142823291 then
   loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
 elseif PlaceId == 2788229376 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua"))()
 elseif PlaceId == 914010731 then
   loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua")))()
 
