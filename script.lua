ffd75aefe7db5a6012f532ab7c248dc72a659d2f
KyleJohnGamingPH <102275738+KyleJohnGamingPH@users.noreply.github.com>
2022-04-06 20:23:50 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,6 +4,7 @@ game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Te
 local PlaceId = game.PlaceId
 
 if PlaceId == 2753915549 or PlaceId == 4442272183 == 7449423635 then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/KuppaHX/MUKUROHUB/main/psxMukuro.lua")()
   
   else
 		game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Notice", Text = "Game is currently not supported"})
