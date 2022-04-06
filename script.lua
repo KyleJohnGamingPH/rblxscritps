ba570513dc0fd4a30d2b734dd67933f044add409
KyleJohnGamingPH <102275738+KyleJohnGamingPH@users.noreply.github.com>
2022-04-06 20:20:33 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,11 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Compiled", Text = "By Kyle.#3427"})
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Text = "1.5"})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Compiled", Text = "By KyleJohn#3427"})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Version", Text = "1.0"})
 
 local PlaceId = game.PlaceId
 
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SHAREHACK/bloxfruit/main/free%22))()
-elseif PlaceId == 142823291 then
-  loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
-elseif PlaceId == 2788229376 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua"))()
-elseif PlaceId == 914010731 then
-  loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua")))()
-
- else
+if PlaceId == 2753915549 or PlaceId == 4442272183 == 7449423635 then
   
- end
+  else
+		game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Notice", Text = "Game is currently not supported"})
+
+  end
